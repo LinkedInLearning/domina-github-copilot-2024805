0f10ffeee0cc6a9c24e6de8167faf849ab03c0b2
Carlos Solis <carlos.solis.d@gmail.com>
2025-07-02 03:55:45 -0400

    resuelto

--- src/pages/Contacto.test.jsx
+++ src/pages/Contacto.test.jsx
@@ -18,4 +18,25 @@ describe('Contacto Component', () => {
         fireEvent.change(nameInput, { target: { value: 'Test Name' } });
         expect(nameInput.value).toBe('Test Name');
     });
+
+    it('limpia los datos del formulario al hacer clic en el botón de borrado', () => {
+        render(<Contacto />);
+        
+        // Arrange: Llenar los campos del formulario
+        const nameInput = screen.getByLabelText('Nombre:');
+        const emailInput = screen.getByLabelText('Correo Electrónico:');
+        const messageInput = screen.getByLabelText('Mensaje:');
+        fireEvent.change(nameInput, { target: { value: 'Test Name' } });
+        fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+        fireEvent.change(messageInput, { target: { value: 'Test Message' } });
+
+        // Act: Hacer clic en el botón de borrado
+        const clearButton = screen.getByText('Borrar Datos');
+        fireEvent.click(clearButton);
+
+        // Assert: Verificar que los campos estén vacíos
+        expect(nameInput.value).toBe('');
+        expect(emailInput.value).toBe('');
+        expect(messageInput.value).toBe('');
+    });
 });
